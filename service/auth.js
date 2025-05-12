1b13646949dce5b7594bb1896226eba4edb22921
Rahul Gangwar <rahulgangwar2625@gmail.com>
2025-05-13 00:29:39 +0530

    JWT authentication

--- service/auth.js
+++ service/auth.js
@@ -1,14 +1,36 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Rahul@143";
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
-} 
-
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-module.exports = {
-    setUser,
-    getUser,
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
+}
+
+const authenticateToken = (req, res, next) => {
+  const token = req.cookie.token || req.headers.authorization?.split(" ")[1];
+  if (!token) return res.status(401).json({ error: "Unauthorized" });
+
+  try {
+    const user = jwt.verify(token, secret);
+    req.user = user; // Set user data in the request object
+    next();
+  } catch (error) {
+    return res.status(403).json({ error: "Invalid token" });
+  }
 };
+
+module.exports = { setUser, getUser, authenticateToken };
